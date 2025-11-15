4fbff3193426f80ea9f9ad968d7112880a10f58a
Imama-M <imama.meraj@ontariotechu.net>
2025-11-15 18:36:46 -0500

    fixed buttons and little errors

--- server/routes/media.js
+++ server/routes/media.js
@@ -1,129 +1,71 @@
 let express = require('express');
 let router = express.Router();
-let mongoose = require('mongoose');
 let Media = require('../models/media');
 
-// get --> Extract & read something
-// post --> post something
-// put --> Edit/Update some data
-// delete --> Delete the data
-// CRUD --> Create, Read, Update & Delete
+// displays the add page
+router.get('/add', (req, res) => {
+    res.render('Media/add', { title: 'Add Review' });
+});
 
-// Get route for the media list - Read Operation
-router.get('/',async(req,res,next)=>{
-    try
-    {
-        const mediaList = await Media.find();
-        //console.log(MediaList);
-        res.render('Media/list',{
-            title:'Media',
-            mediaList: mediaList
-        })
-    }
-    catch(err)
-    {
-        console.error(err);
-        res.render('Media/list',{
-            title:'Media',
-            error:'Error on server'
-        })
-    }
-})
-
-// Get route for displaying the Add Page - Create Operation
-router.get('/add',async(req,res,next)=>{
-    try{
-        res.render('Media/add',{
-            title:'Add a Media'
-        })
-    }
-    catch(err)
-    {
-        console.error(err);
-        res.render('Media/add',{
-            title:'Error',
-            error:'Error on server'
-        })
-    }
-})
-// Post route for processing the Add Page - Create Operation
-router.post('/add',async(req,res,next)=>{
-    try
-    {
-        let newMedia = Media({
-            "Media Type":req.body.type,
-            "Name":req.body.name,
-            "Description":req.body.description,
-            "Rating (out of 5 stars) ": req.body.rating,
-            "Review":req.body.review
-        });
-        Media.create(newMedia).then(()=>{
-            res.redirect('/media')
-        })
-    }
-    catch(err)
-    {
-        console.error(err);
-        res.render('Media/add',{
-            error:'Error on server'
-        })
-    }
-})
-// Get route for displaying the Edit Page - Update Operation
-router.get('/edit/:id',async(req,res,next)=>{
-    try
-    { 
-        const id = req.params.id;
-        const mediatoEdit = await Media.findById(id);
-        res.render('Media/edit', 
-            { 
-                title: 'Edit Media Review',
-                media: mediatoEdit
-            }
-);
-
-    }
-
-    catch(err)
-    {
-        console.log(err);
-        next(err);
-    }
-
-})
-
-// Post route for processing the Edit Page - Update Operation
-router.post('/edit/:id', async(req, res, next) => {
+// processing the actual form and data
+router.post('/add', async (req, res) => {
     try {
-        const id = req.params.id;
-        let updateMedia = {
-            "_id": id,
-            "Media Type": req.body.type,      
+        let newMedia = {
+            "Media Type": req.body.type,
             "Name": req.body.name,
             "Description": req.body.description,
             "Rating (out of 5 stars) ": req.body.rating,
             "Review": req.body.review
         };
-        Media.findByIdAndUpdate(id, updateMedia); 
-        res.redirect('/media');
-    } 
-    catch(err) {
-        console.log(err);
-        next(err);
+
+        await Media.create(newMedia);
+        res.redirect('/');   
+    } catch (err) {
+        console.error(err);
+        res.render('Media/add', { error: 'Error on server' });
     }
 });
 
-
-
-// Get route for performing delete operation - Delete Operation
-router.get('/delete/:id',async(req,res,next)=>{
-    try
-    {
-        const id = req.params.id;
-        Media.deleteOne({_id:id}).then(()=>{
-            res.redirect('/media')
-        })  
+// displays the EDIT page to edit the forms 
+router.get('/edit/:id', async (req, res) => {
+    try {
+        //find the id for the item and pulls it up to edit then after editing sens user back home
+        const media = await Media.findById(req.params.id);
+        res.render('Media/edit', { title: 'Edit Media Review', media });
+    } catch (err) {
+        console.error(err);
+        res.redirect('/');
     }
+});
 
-})
+// EDIT form processing which updates the edits
+router.post('/edit/:id', async (req, res) => {
+    try {
+        await Media.findByIdAndUpdate(req.params.id, {
+            "Media Type": req.body.type,
+            "Name": req.body.name,
+            "Description": req.body.description,
+            "Rating (out of 5 stars) ": req.body.rating,
+            "Review": req.body.review
+        });
+
+        res.redirect('/');   
+    } catch (err) {
+        console.error(err);
+        res.redirect('/');
+    }
+});
+
+// for deleting items
+router.get('/delete/:id', async (req, res) => {
+    try {
+        //deletes a whole item then sends user back to home page
+        await Media.deleteOne({ _id: req.params.id });
+        res.redirect('/');   
+    } catch (err) {
+        console.error(err);
+        res.redirect('/');
+    }
+});
+
 module.exports = router;
